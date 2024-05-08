ff9e501732966af7b9396aaa2f35ee6aa63b14c2
donMaleesha <lochanamt@gmail.com>
2024-05-08 09:55:10 +0530

    remove the isset statement from index.php and copied to dbconn.php

--- index.php
+++ index.php
@@ -2,33 +2,6 @@
 
 include "dbconn.php"; //link database connection
 
-if (isset($_POST['submit'])) {
-
-    $fname = $_POST['firstName'];
-    $lname = $_POST['lastName'];
-    $email = $_POST['emailAddress'];
-    $mobile = $_POST['mobilenumber'];
-
-    //access the database
-
-
-    //insert query
-    $sql = "INSERT INTO users (firstName, lastName, emailAddress, mobileNumber) VALUES ('$fname', '$lname', '$email', '$mobile') ";
-
-    //checking
-    $result = mysqli_query($conn, $sql);
-
-    if ($result) {
-
-        header("location: read.php"); //linked to read.php
-
-    } else {
-
-        die("Connection failed " . mysqli_connect_error());
-    }
-
-}
-
 ?>
 
 <!DOCTYPE html>

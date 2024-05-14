96ba04d0cd3b446f8d172a5156375278718d4158
donMaleesha <lochanamt@gmail.com>
2024-05-14 11:25:48 +0530

    database connection

--- dbconn.php
+++ dbconn.php
@@ -1,10 +1,11 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "php_crud";
+$servername = "localhost"; // Define the server name where the database is hosted.
+$username = "root"; // Define the username for accessing the database.
+$password = ""; // Define the password for the database user.
+$dbname = "php_crud"; // Define the name of the database you want to connect to.
 
+// Check if the connection was successful.
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
